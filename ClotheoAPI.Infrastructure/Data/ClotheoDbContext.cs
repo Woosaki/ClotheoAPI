0c5ca0818d4a72fcec7655bb45af369d6b9d803e
Kamil Szczukowski <kamil.szczukowski@interia.pl>
2025-04-09 16:25:35 +0200

    Add EF Core and PostgreSQL support with ClotheoDbContext

---
+++ ClotheoAPI.Infrastructure/Data/ClotheoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ClotheoAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClotheoAPI.Infrastructure.Data;
+
+public class ClotheoDbContext(DbContextOptions<ClotheoDbContext> options) : DbContext(options)
+{
+    public DbSet<User> User { get; set; }
+    public DbSet<Listing> Listing { get; set; }
+    public DbSet<Message> Message { get; set; }
+    public DbSet<Photo> Photo { get; set; }
+    public DbSet<Category> Category { get; set; }
+}
