d6c8f335c94b2eeb815a6885b2d93cbfaff898fc
Kamil Szczukowski <kamil.szczukowski@interia.pl>
2025-04-10 16:38:15 +0200

    Update database connection and improve migration handling

--- ClotheoAPI/Program.cs
+++ ClotheoAPI/Program.cs
@@ -3,17 +3,39 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-var connectionString = builder.Configuration.GetConnectionString("ClotheoDbContext");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ClotheoDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString(connectionString)));
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var dbContext = scope.ServiceProvider.GetRequiredService<ClotheoDbContext>();
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+if (await dbContext.Database.CanConnectAsync())
+{
+    logger.LogInformation("Database connection successful.");
+
+    var pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
+    if (pendingMigrations.Any())
+    {
+        logger.LogInformation($"Found {pendingMigrations.Count()} pending migrations. Applying...");
+        await dbContext.Database.MigrateAsync();
+        logger.LogInformation("Migrations applied successfully.");
+    }
+    else
+    {
+        logger.LogInformation("No pending migrations to apply.");
+    }
+}
+else
+{
+    logger.LogError("Database connection failed.");
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -27,4 +49,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
