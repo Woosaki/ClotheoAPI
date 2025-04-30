3effa92501dcb7699a50e29b2dd1772670938dfc
Kamil Szczukowski <kamil.szczukowski@interia.pl>
2025-04-30 16:19:12 +0200

    Add user context validation to command handlers

--- ClotheoAPI.Application/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ ClotheoAPI.Application/Users/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,3 +1,4 @@
+using ClotheoAPI.Application.Auth.Context;
 using ClotheoAPI.Domain.Entities;
 using ClotheoAPI.Domain.Exceptions;
 using ClotheoAPI.Domain.Repositories;
@@ -5,12 +6,21 @@ using MediatR;
 
 namespace ClotheoAPI.Application.Users.Commands.DeleteUser;
 
-public class DeleteUserCommandHandler(IUserRepository userRepository) : IRequestHandler<DeleteUserCommand>
+public class DeleteUserCommandHandler(IUserRepository userRepository, IUserContext userContext)
+    : IRequestHandler<DeleteUserCommand>
 {
     public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
     {
         var userToDelete = await userRepository.GetByIdAsync(request.Id)
             ?? throw new NotFoundException(nameof(User), request.Id);
+
+        var currentUser = userContext.GetCurrentUser()
+            ?? throw new UnauthorizedException();
+
+        if (currentUser.Id != userToDelete.Id && !currentUser.IsAdmin)
+        {
+            throw new ForbiddenException();
+        }
 
         await userRepository.DeleteAsync(userToDelete);
     }
--- ClotheoAPI.Application/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
+++ ClotheoAPI.Application/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -1,3 +1,4 @@
+using ClotheoAPI.Application.Auth.Context;
 using ClotheoAPI.Domain.Entities;
 using ClotheoAPI.Domain.Exceptions;
 using ClotheoAPI.Domain.Repositories;
@@ -5,12 +6,21 @@ using MediatR;
 
 namespace ClotheoAPI.Application.Users.Commands.UpdateUser;
 
-public class UpdateUserCommandHandler(IUserRepository userRepository) : IRequestHandler<UpdateUserCommand>
+public class UpdateUserCommandHandler(IUserRepository userRepository, IUserContext userContext)
+    : IRequestHandler<UpdateUserCommand>
 {
     public async Task Handle(UpdateUserCommand request, CancellationToken cancellationToken)
     {
         var userToUpdate = await userRepository.GetByIdAsync(request.Id)
             ?? throw new NotFoundException(nameof(User), request.Id);
+
+        var currentUser = userContext.GetCurrentUser()
+            ?? throw new UnauthorizedException();
+
+        if (currentUser.Id != userToUpdate.Id && !currentUser.IsAdmin)
+        {
+            throw new ForbiddenException();
+        }
 
         if (request.Username is not null)
         {
