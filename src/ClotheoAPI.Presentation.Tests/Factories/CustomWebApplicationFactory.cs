7f5e873147765541da9020db1a573fe9fbeedaee
Kamil Szczukowski <kamil.szczukowski@interia.pl>
2025-05-09 14:56:03 +0200

    Add CustomWebApplicationFactory for integration tests

---
+++ src/ClotheoAPI.Presentation.Tests/Factories/CustomWebApplicationFactory.cs
@@ -0,0 +1,54 @@
+﻿using ClotheoAPI.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using Xunit;
+
+namespace ClotheoAPI.Presentation.Tests.Factories;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+            .WithDatabase("testdb")
+            .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<ClotheoDbContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<ClotheoDbContext>(options =>
+            {
+                options.UseNpgsql(_dbContainer.GetConnectionString());
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+
+            var db = scopedServices.GetRequiredService<ClotheoDbContext>();
+            db.Database.Migrate();
+
+        });
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+        await _dbContainer.DisposeAsync();
+    }
+}
