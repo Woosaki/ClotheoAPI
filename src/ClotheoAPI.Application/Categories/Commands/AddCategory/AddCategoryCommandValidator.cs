40d63fe61a561499acd2474f043395ddcd65fc82
Kamil Szczukowski <kamil.szczukowski@interia.pl>
2025-05-12 14:44:57 +0200

    Add category command validator and its unit tests

---
+++ src/ClotheoAPI.Application/Categories/Commands/AddCategory/AddCategoryCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace ClotheoAPI.Application.Categories.Commands.AddCategory;
+
+public class AddCategoryCommandValidator : AbstractValidator<AddCategoryCommand>
+{
+    public AddCategoryCommandValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("Category name is required.")
+            .Length(3, 50).WithMessage("Category name must be between 3 and 50 characters.");
+    }
+}
---
+++ tests/ClotheoAPI.Application.Tests/Categories/Commands/AddCategory/AddCategoryCommandValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.TestHelper;
+using Xunit;
+
+namespace ClotheoAPI.Application.Categories.Commands.AddCategory.Tests;
+
+public class AddCategoryCommandValidatorTests
+{
+    private readonly AddCategoryCommandValidator _validator;
+
+    public AddCategoryCommandValidatorTests()
+    {
+        _validator = new AddCategoryCommandValidator();
+    }
+
+    [Fact]
+    public void AddCategory_Should_not_have_errors_when_name_is_valid()
+    {
+        var command = new AddCategoryCommand { Name = "t-shirt" };
+        var result = _validator.TestValidate(command);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public void AddCategory_Should_have_error_when_name_is_empty()
+    {
+        var command = new AddCategoryCommand { Name = "" };
+        var result = _validator.TestValidate(command);
+
+        result.ShouldHaveValidationErrorFor(x => x.Name)
+            .WithErrorMessage("Category name is required.");
+    }
+
+    [Theory]
+    [InlineData("ab")]
+    [InlineData("ThisCategoryNameIsTooLongAndExceedsMaximumAllowedLength")]
+    public void AddCategory_Should_have_error_when_name_length_is_invalid(string name)
+    {
+        var command = new AddCategoryCommand { Name = name };
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Name)
+            .WithErrorMessage("Category name must be between 3 and 50 characters.");
+    }
+}
